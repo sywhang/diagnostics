4aaa4791452705e1f87230cfeba88436b6501519
Sung Yoon Whang <suwhang@microsoft.com>
2020-10-26 15:52:51 -0700

    dotnet-trace listen

---
+++ src/Tools/dotnet-trace/CommandLine/Commands/ListenCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Diagnostics.Tools.Trace.CommandLine.Commands
+{
+    internal static class ListenCommandHandler
+    {
+
+        private static async Task<int> Listen(CancellationToken ct, IConsole console, FileInfo port)
+        {
+
+        }
+    }
+}
